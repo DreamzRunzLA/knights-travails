1f231994de7bc0e6c861883bce9ad7524d2a4c60
DreamzRunzLA <salmaan.a.qadir@gmail.com>
2019-10-11 16:32:05 -0600

    Made some progress on pos validator

--- knightpathfinder.rb
+++ knightpathfinder.rb
@@ -3,9 +3,20 @@ require_relative 'polytreenode.rb'
 class KnightPathFinder
     def initialize(starting_position)
         @starting_position = starting_position
+        @size = 8
     end
 
     def find_path(start_pos, end_pos)
 
+    end
+
+    def valid_pos?(pos)
+        pos.is_a?(Array) &&
+          pos.length == 2 &&
+          pos.all? { |x| x.between?(0, @size - 1) }
+    end
+
+    def parse_pos(string)
+        string.split(",").map { |char| Integer(char) }
     end
 end
