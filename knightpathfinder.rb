4af69d1c1b44fedf765fa775ccb4473354dfaaf0
DreamzRunzLA <salmaan.a.qadir@gmail.com>
2019-10-11 15:16:19 -0600

    Third commit

--- knightpathfinder.rb
+++ knightpathfinder.rb
@@ -1,9 +1,11 @@
-class knightpathfinder
+require_relative 'polytreenode.rb'
+
+class KnightPathFinder
     def initialize(starting_position)
         @starting_position = starting_position
     end
 
-    def find_path(start, end)
+    def find_path(start_pos, end_pos)
 
     end
 end
