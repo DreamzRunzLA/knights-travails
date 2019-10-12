785519560db9700d41b5717247bedfddbaeb1605
DreamzRunzLA <salmaan.a.qadir@gmail.com>
2019-10-12 14:53:24 -0600

    got valid moves working

--- knightpathfinder.rb
+++ knightpathfinder.rb
@@ -4,6 +4,27 @@ class KnightPathFinder
     def initialize(starting_position)
         @starting_position = starting_position
         @size = 8
+        @root_node = PolyTreeNode.new(starting_position)
+        build_move_tree
+        @considered_positions = [@root_node]
+    end
+
+    def self.valid_moves(pos)
+        moves = []
+        moves << PolyTreeNode.new([pos[0] - 1, pos[1] + 2])
+        moves << PolyTreeNode.new([pos[0] - 1, pos[1] - 2])
+        moves << PolyTreeNode.new([pos[0] - 2, pos[1] + 1])
+        moves << PolyTreeNode.new([pos[0] - 2, pos[1] - 1])
+        moves << PolyTreeNode.new([pos[0] + 1, pos[1] + 2])
+        moves << PolyTreeNode.new([pos[0] + 1, pos[1] - 2])
+        moves << PolyTreeNode.new([pos[0] + 2, pos[1] + 1])
+        moves << PolyTreeNode.new([pos[0] + 2, pos[1] - 1])
+
+        moves = moves.select do |move|
+            move.value[0] < 8 && move.value[1] < 8 && move.value[0] > 0 && move.value[1] > 0
+        end
+
+        return moves
     end
 
     def find_path(start_pos, end_pos)
@@ -19,4 +40,10 @@ class KnightPathFinder
     def parse_pos(string)
         string.split(",").map { |char| Integer(char) }
     end
+
+    def build_move_tree
+
+    end
+
+
 end
